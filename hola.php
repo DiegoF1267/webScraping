d2ab5f7f41380adc9b59f0295f475b3044046e1f
DiegoF1267 <diegolimon123@gmail.com>
2024-10-22 01:55:50 -0300

    primer commit

---
+++ hola.php
@@ -0,0 +1,13 @@
+<?php
+echo "2";
+$diego =1;
+$diego2=2;
+$array=array();
+array_push($array,$diego);
+array_push($array,$diego2);
+
+for($i=0;$i<count($array);$i++){
+
+    echo "<br>"."numero: ".$array[$i]."<br>";
+
+}
